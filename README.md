a23ec762fcdf599e5897c3ce76b8d35a308f1443
puddleglumm <matthew.raehl@gmail.com>
2022-05-30 14:41:15 -0400

    Setup minesweeper kata

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # katas
+
+New katas generated with maven archetype
+uk.co.markg.archetypes:java11-junit5:1.0
---
+++ minesweeper/src/main/java/com/github/puddleglumm/App.java
@@ -0,0 +1,9 @@
+package com.github.puddleglumm;
+
+public class App {
+    
+    public static void main(String[] args) {
+        System.out.println("It's alive!");
+    }
+    
+}
